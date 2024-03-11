665e9e5ea2a1cebbc6a86acafad31c7e70ec40c6
Juan Carlos Cruz <j.cruzb@outlook.com>
2024-03-11 17:10:33 +0100

    fixing delete user

--- src/Services/UsersService.js
+++ src/Services/UsersService.js
@@ -12,7 +12,7 @@ export const getUsersData = () => http.get('/users/data');
 export const getUserDetail = (id) => http.get(`/users/${id}`);
 
 
-export const deleteUser = (id) => http.delete(`/users/users/${id}`);
+export const deleteUser = (id) => http.delete(`/users/${id}`);
 
 export const updateUser = (id, user) => http.put(`/users/${id}`, user);
 
